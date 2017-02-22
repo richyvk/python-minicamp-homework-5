09cea2137bc9b137a08d025c195e84957f4c86cb
Richard Vankoningsveld <richyvk@gmail.com>
2017-02-22 20:43:45 +1000

    PEP8 cleanup

--- server.py
+++ server.py
@@ -3,41 +3,42 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
 
-@app.route('/new-pet', methods = ['POST'])
+
+@app.route('/new-pet', methods=['POST'])
 def newAnimal():
-	connection = sqlite3.connect('database.db')
-	cursor = connection.cursor()
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
 
-	name = request.form['name']
-	species = request.form['species']
-	age = request.form['age']
+    name = request.form['name']
+    species = request.form['species']
+    age = request.form['age']
 
-	try:
-		cursor.execute('INSERT INTO pets (name, species, age) VALUES (?, ?, ?)', (name, species, age))
-		connection.commit()
-		message = 'successfully inserted'
-	except Exception as err:
-		print(err)
-		connection.rollback()
-		message = 'an error occured'
-	finally:
-		connection.close()
-		return message
+    try:
+        cursor.execute('INSERT INTO pets (name, species, age) VALUES (?, ?, ?)', (name, species, age))
+        connection.commit()
+        message = 'successfully inserted'
+    except Exception as err:
+        print(err)
+        connection.rollback()
+        message = 'an error occured'
+    finally:
+        connection.close()
+        return message
+
 
 @app.route('/pets')
 def pets():
-	connection = sqlite3.connect('database.db')
-	cursor = connection.cursor()
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
 
-	cursor.execute('SELECT * FROM pets')
-	petsList = cursor.fetchall()
+    cursor.execute('SELECT * FROM pets')
+    petsList = cursor.fetchall()
 
-	connection.close()
+    connection.close()
 
-	return jsonify(petsList)
-
-app.run(debug = True)
+    return jsonify(petsList)
