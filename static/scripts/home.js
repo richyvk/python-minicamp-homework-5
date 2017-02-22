041e6af8680624e6cbee9ab1d0dce441779b888e
Richard Vankoningsveld <richyvk@gmail.com>
2017-02-23 08:01:29 +1000

    Add form validation JS

---
+++ static/scripts/home.js
@@ -0,0 +1,22 @@
+function validateForm() {
+	var name = document.getElementById('nameInput').value;
+	var species = document.getElementById('speciesInput').value;
+	var age = document.getElementById('ageInput').value;
+
+	if (!name.length || !species.length || !age.length) {
+		alert('Fields must not be empty');
+		return false;
+	}
+
+	if (Boolean(Number(species))) {
+		alert('Not a valid species');
+		return false;
+	}
+
+	if (!Boolean(Number(age))) {
+		alert('Age must be a number');
+		return false;
+	}
+
+	return true;
+}
